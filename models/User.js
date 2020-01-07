f7ffcd4005605894d96226c1ab60817b539c2b45
MAZAKPE <makpalyy@gmail.com>
2020-01-07 15:04:54 +0200

    modified the user model

--- models/User.js
+++ models/User.js
@@ -19,7 +19,7 @@ const UserSchema = new mongoose.Schema({
     },
     role: {
         type: String,
-        enum: ["user", "publisher", "admin"],
+        enum: ["user", "vendor", "admin"],
         default: "user"
     },
     password: {
