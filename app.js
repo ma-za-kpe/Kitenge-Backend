122d93cc39ccffbf4a5ad86e16a508460333fd91
MAZAKPE <makpalyy@gmail.com>
2020-03-02 13:33:32 +0200

    added vendors page

--- app.js
+++ app.js
@@ -74,7 +74,7 @@ app.use(fileupload());
 
 app.use("/", indexRouter);
 app.use("/developer", developerRouter);
-app.use("/api/v1/vendors", vendorRouter);
+app.use("/vendors", vendorRouter);
 app.use("/api/v1/admin", adminRouter);
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/kiosk", kioskRouter);
